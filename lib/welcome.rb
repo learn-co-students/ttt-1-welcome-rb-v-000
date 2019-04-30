0ddbcce9c655704b0e14a5be9424602cd57a3e2e
jeya kasinathan <kasinathanjeya@gmail.com>
2019-04-30 02:24:08 +0000

    Done.

--- lib/welcome.rb
+++ lib/welcome.rb
@@ -1,6 +1,6 @@
 # Edit this file to output "Welcome to Tic Tac Toe!"
 # You can see what this file does by running:
-# ruby lib/welcome.rb from your terminal.
+# ruby puts "welcome to Tic Tac Toe" lib/welcome.rb from your terminal.
 puts "Welcome to Tic Tac Toe!"
 
 learn submit
--- spec/welcome_spec.rb
+++ spec/welcome_spec.rb
@@ -3,6 +3,7 @@ describe "lib/welcome.rb" do
     expect($stdout).to receive(:puts).with("Welcome to Tic Tac Toe!"), "Make sure lib/welcome.rb has code that can output 'Welcome to Tic Tac Toe!' exactly."
 
     load '.ruby /lib/welcome.rb'
+    puts "welcome to Tic Tac Toe"
 
   end
 end
